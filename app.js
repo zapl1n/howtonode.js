a538aa231d74516354a4d46bf81fe17dee16f7f9
Toomas <toomas.heljula@khk.ee>
2022-03-21 12:55:21 +0200

    Myfirst

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World!');
-}).listen(8080);
+exports.myDateTime = function () {
+    return Date();
+};
