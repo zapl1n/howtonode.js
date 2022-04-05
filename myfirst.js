ec7269189b257fdc71368646136255c0e643d238
Toomas <toomas.heljula@khk.ee>
2022-04-05 19:09:49 +0300

    Module

--- myfirst.js
+++ myfirst.js
@@ -1,7 +1,6 @@
 var http = require('http');
 var dt = require('./myfirstmodule');
 
-
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write("The date and time is currently: " + dt.myDateTime());
