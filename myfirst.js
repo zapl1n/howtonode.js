ecb487fb880a7dd6552eb4a4b0356535fb1cbd3b
Toomas <toomas.heljula@khk.ee>
2022-04-15 18:07:47 +0300

    Module

--- myfirst.js
+++ myfirst.js
@@ -1,8 +1,7 @@
 var http = require('http');
-var dt = require('./myfirstmodule');
 
+//create a server object:
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("The date and time is currently: " + dt.myDateTime());
-    res.end();
-}).listen(8080);
+    res.write('Hello World!'); //write a response to the client
+    res.end(); //end the response
+}).listen(8080); //the server object listens on port 8080
