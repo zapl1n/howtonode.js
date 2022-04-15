8eeb02201750659a05284e7626280550a87308cc
Toomas <toomas.heljula@khk.ee>
2022-04-15 18:30:50 +0300

    Module

--- demo_uppercase.js
+++ demo_uppercase.js
@@ -1,13 +1,10 @@
-var events = require('events');
-var eventEmitter = new events.EventEmitter();
+var http = require('http');
 
-//Create an event handler:
-var myEventHandler = function () {
-    console.log('I hear a scream!');
-}
-
-//Assign the event handler to an event:
-eventEmitter.on('scream', myEventHandler);
-
-//Fire the 'scream' event:
-eventEmitter.emit('scream');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    return res.end();
+}).listen(8080);
