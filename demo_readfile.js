20c2b32387236174768982faefaf97a1d643a44d
Toomas <toomas.heljula@khk.ee>
2022-04-15 18:20:18 +0300

    Module

--- demo_readfile.js
+++ demo_readfile.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 
-fs.writeFile('mynewfile3.txt', 'Hello content!', function (err) {
+fs.appendFile('mynewfile1.txt', ' This is my text.', function (err) {
     if (err) throw err;
-    console.log('Saved!');
+    console.log('Updated!');
 });
