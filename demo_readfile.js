fe90f4dc8b67dcfa12eab63b4097ce4f46a74c8d
Toomas <toomas.heljula@khk.ee>
2022-04-15 18:18:30 +0300

    Module

--- demo_readfile.js
+++ demo_readfile.js
@@ -1,9 +1,6 @@
-var http = require('http');
 var fs = require('fs');
-http.createServer(function (req, res) {
-    fs.readFile('demofile1.html', function(err, data) {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
-    });
-}).listen(8080);
+
+fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
+    if (err) throw err;
+    console.log('Saved!');
+});
