4662de29c9d7e8e38202322f9f7b1c93f427f444
Toomas <toomas.heljula@khk.ee>
2022-04-15 18:29:29 +0300

    Module

--- demo_uppercase.js
+++ demo_uppercase.js
@@ -1,7 +1,5 @@
-var http = require('http');
-var uc = require('upper-case');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(uc.upperCase("Hello World!"));
-    res.end();
-}).listen(8080);
+var fs = require('fs');
+var rs = fs.createReadStream('./demofile.txt');
+rs.on('open', function () {
+    console.log('The file is open');
+});
