68439ff24933d89705431f4e42d28e5ff484179b
Toomas <toomas.heljula@khk.ee>
2022-04-15 18:22:35 +0300

    Module

--- demo_readfile.js
+++ demo_readfile.js
@@ -1,6 +1,10 @@
-var fs = require('fs');
+var url = require('url');
+var adr = 'http://localhost:8080/default.htm?year=2017&month=february';
+var q = url.parse(adr, true);
 
-fs.rename('mynewfile1.txt', 'myrenamedfile.txt', function (err) {
-    if (err) throw err;
-    console.log('File Renamed!');
-});
+console.log(q.host); //returns 'localhost:8080'
+console.log(q.pathname); //returns '/default.htm'
+console.log(q.search); //returns '?year=2017&month=february'
+
+var qdata = q.query; //returns an object: { year: 2017, month: 'february' }
+console.log(qdata.month); //returns 'february'
