b053d78aef511b54789986af0d22e2ee00537c74
Toomas <toomas.heljula@khk.ee>
2022-04-15 18:29:55 +0300

    Module

--- demo_uppercase.js
+++ demo_uppercase.js
@@ -1,5 +1,13 @@
-var fs = require('fs');
-var rs = fs.createReadStream('./demofile.txt');
-rs.on('open', function () {
-    console.log('The file is open');
-});
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
+
+//Create an event handler:
+var myEventHandler = function () {
+    console.log('I hear a scream!');
+}
+
+//Assign the event handler to an event:
+eventEmitter.on('scream', myEventHandler);
+
+//Fire the 'scream' event:
+eventEmitter.emit('scream');
